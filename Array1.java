94818fa6f5e2fdde8a05d198e320fd492bef4fef
Keeru <rvkeerthana.katta@gmail.com>
2021-06-17 16:28:44 +0200

    adding two files

---
+++ Array1.java
@@ -0,0 +1,32 @@
+class Array1
+{
+public static int[] post4(int[] a) 
+{
+  for(int i=0;i<a.length;i++)
+  {
+    for(int j=i+1;j<a.length;j++)
+    {
+      if(a[i]==a[j])
+      {
+        a[j]=0;
+      }
+    }
+  }
+  
+  printarray(a);
+  
+  return a;
+}
+public static void printarray(int a[])
+{
+  for(int i=0;i<a.length;i++)
+  {
+    System.out.println(a[i]);
+  }
+}
+public static void main(String[] args) 
+{
+  int[] a1={1,1,1,4,2,4,5};
+  post4(a1);
+}
+}
